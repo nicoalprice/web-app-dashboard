93651cd50bccec07e1ca82fe11b9b7f269867ebf
Nicoal Price <nicoalprice@gmail.com>
2016-08-31 16:57:58 -0400

    added js file with notes

---
+++ app.js
@@ -0,0 +1,4 @@
+// Use JS to allow you to submit the form and display a confirmation the message was sent. You won't actually submit the form, just simulate the action using JavaScript.
+
+
+// Use JS to display error messages if user isn’t selected or message field is empty.
