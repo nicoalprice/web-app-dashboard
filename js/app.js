c3e929544e290233d5b0a6dd882ede7d9e0b1d15
Nicoal Price <nicoalprice@gmail.com>
2016-09-20 10:03:31 -0400

    css styling and js files

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/* Use JS to allow you to submit the form and display a confirmation the message was sent. You won't actually submit the form, just simulate the action using JavaScript.*/
+function sendMessage() {
+	var sendButton = document.getElementById("send-button");
+//when user clicks send
+// pop-up message
+// text "Your message has been sent."
+}
+
+
+/* Use JS to display error messages if user isn’t selected or message field is empty.*/
+function errorMessage() {
+	//(if no user is selected || message field is empty) {
+		//display error message
+	}
+}
--- js/charts.js
+++ js/charts.js
@@ -40,9 +40,20 @@ var myChart = new Chart(dailyTraffic, {
 });
 
 
-// Use JS to allow you to submit the form and display a confirmation the message was sent. You won't actually submit the form, just simulate the action using JavaScript.
+/* Use JS to allow you to submit the form and display a confirmation the message was sent. You won't actually submit the form, just simulate the action using JavaScript.*/
+function sendMessage() {
+	var sendButton = document.getElementById("send-button");
+//when user clicks send
+// pop-up message
+// text "Your message has been sent."
+}
 
 
-// Use JS to display error messages if user isn’t selected or message field is empty.
+/* Use JS to display error messages if user isn’t selected or message field is empty.*/
+function errorMessage() {
+	//(if no user is selected || message field is empty) {
+		//display error message
+	}
+}
 
 
