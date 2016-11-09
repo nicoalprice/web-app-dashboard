c85c0671eea0842043efeb1073bb80b13a11637e
Nicoal Price <nicoalprice@gmail.com>
2016-11-09 11:39:31 -0500

    added line chart toggling

--- js/charts.js
+++ js/charts.js
@@ -1,36 +1,33 @@
 /* CHARTS */
 
-/* Weekly Line Chart */
+/* Traffic Line Chart */
+
+/* Change class when buttons above traffic chart are clicked. */
+
+
 var traffic = document.getElementById("trafficChart");
 
-var trafficChart = new Chart(traffic, {
-	type: 'line',
-	data: {
-		labels: ["", "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31", ""],
-		datasets: [{
-			data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000],
-			backgroundColor: [
-				'#e2e3f6',
-			],
-			borderColor: [
-				'rgba(115, 119, 191,1)'
-			],
-			borderWidth: 1,
-			lineTension: 0,
-			pointBackgroundColor: 'white',
-			pointBorderColor: '#7377bf',
-			pointBorderWidth: 1.5,
-			pointRadius: 5,
-			},
+// Declare variables for traffic chart buttons
+var hourly = $("#hourly-traffic");
+var daily = $("#daily-traffic-button");
+var weekly = $("#weekly-traffic");
+var monthly = $("#monthly-traffic");
+// declare array including all traffic chart buttons
+var buttonArray = [hourly, daily, weekly, monthly];
 
-			{//hourly
-				data: [0, 30, 10, 750, 1230, 150, 650, 100, 12, 50, 20, 15, 21],
-				label: "daily",
+//show weekly chart by default/on page load
+weekly.hasClass("active", function() {
+	var trafficChart = new Chart(traffic, {
+		type: 'line',
+		data: {
+			labels: ["", "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31", ""],
+			datasets: [{
+				data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000],
 				backgroundColor: [
-					'#e2e776',
+					'#e2e3f6',
 				],
 				borderColor: [
-					'rgba(115, 121, 122,1)'
+					'rgba(115, 119, 191,1)'
 				],
 				borderWidth: 1,
 				lineTension: 0,
@@ -38,47 +35,251 @@ var trafficChart = new Chart(traffic, {
 				pointBorderColor: '#7377bf',
 				pointBorderWidth: 1.5,
 				pointRadius: 5,
-				hidden: true
-			}
+				}
+			]
 
-				   {//daily
-				data: [0, 340, 100, 75, 1230, 150, 650, 1000, 100, 500, 200, 150, 200],
-				label: "daily",
-				backgroundColor: [
-					'#e2e776',
-				],
-				borderColor: [
-					'rgba(115, 121, 122,1)'
-				],
-				borderWidth: 1,
-				lineTension: 0,
-				pointBackgroundColor: 'white',
-				pointBorderColor: '#7377bf',
-				pointBorderWidth: 1.5,
-				pointRadius: 5,
-				hidden: true
-			}
-		]
-
-	},
-	options: {
-		scales: {
-			yAxes: [{
-				ticks: {
-					max: 2500,
-					min: 0,
-					stepSize: 500,
-				},
-			}],
 		},
-		responsive: true,
-		legend: {
-			display: false
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						max: 2500,
+						min: 0,
+						stepSize: 500,
+					},
+				}],
+			},
+			responsive: true,
+			legend: {
+				display: false
+			}
 		}
-	}
+	});
 });
 
-/* Daily Traffic Chart */
+// On click of a button
+hourly.click(function() {
+	// Remove .active class from all buttons
+	for (i = 0; i < buttonArray.length; i++) {
+		buttonArray[i].removeClass("active");
+	}
+
+	// Add .active class
+	hourly.addClass("active");
+
+	var trafficChart = new Chart(traffic, {
+		type: 'line',
+		data: {
+			labels: ["", "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31", ""],
+			datasets: [{
+				data: [0, 5, 20, 25, 58, 74, 17, 10, 4, 150, 20, 18, 21],
+				backgroundColor: [
+					'#f42426',
+				],
+				borderColor: [
+					'rgba(115, 119, 191,1)'
+				],
+				borderWidth: 1,
+				lineTension: 0,
+				pointBackgroundColor: 'white',
+				pointBorderColor: '#7377bf',
+				pointBorderWidth: 1.5,
+				pointRadius: 5,
+				}
+					]
+
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						max: 2500,
+						min: 0,
+						stepSize: 500,
+					},
+				}],
+			},
+			responsive: true,
+			legend: {
+				display: false
+			}
+		}
+	});
+
+});
+
+// on click of a button
+daily.click(function() {
+	// remove .active class from all buttons
+	for (i = 0; i < buttonArray.length; i++) {
+		buttonArray[i].removeClass("active");
+	}
+
+	// add .active class
+	daily.addClass("active");
+
+	var trafficChart = new Chart(traffic, {
+		type: 'line',
+		data: {
+			labels: ["", "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31", ""],
+			datasets: [{
+				data: [0, 50, 100, 250, 258, 745, 127, 100, 104, 150, 2000, 180, 210],
+				backgroundColor: [
+					'#e24426',
+				],
+				borderColor: [
+					'rgba(115, 119, 191,1)'
+				],
+				borderWidth: 1,
+				lineTension: 0,
+				pointBackgroundColor: 'white',
+				pointBorderColor: '#7377bf',
+				pointBorderWidth: 1.5,
+				pointRadius: 5,
+				}
+					]
+
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						max: 2500,
+						min: 0,
+						stepSize: 500,
+					},
+				}],
+			},
+			responsive: true,
+			legend: {
+				display: false
+			}
+		}
+	});
+
+});
+
+// Weekly Line Chart
+weekly.click(function() {
+	// remove .active class from all buttons
+	for (i = 0; i < buttonArray.length; i++) {
+		buttonArray[i].removeClass("active");
+	}
+
+	// add .active class
+	weekly.addClass("active");
+
+	var trafficChart = new Chart(traffic, {
+		type: 'line',
+		data: {
+			labels: ["", "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31", ""],
+			datasets: [{
+				data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000],
+				backgroundColor: [
+					'#e2e3f6',
+				],
+				borderColor: [
+					'rgba(115, 119, 191,1)'
+				],
+				borderWidth: 1,
+				lineTension: 0,
+				pointBackgroundColor: 'white',
+				pointBorderColor: '#7377bf',
+				pointBorderWidth: 1.5,
+				pointRadius: 5,
+				},
+
+					   {//daily
+					data: [0, 340, 100, 75, 1230, 150, 650, 1000, 100, 500, 200, 150, 200],
+					label: "daily",
+					backgroundColor: [
+						'#e2e776',
+					],
+					borderColor: [
+						'rgba(115, 121, 122,1)'
+					],
+					borderWidth: 1,
+					lineTension: 0,
+					pointBackgroundColor: 'white',
+					pointBorderColor: '#7377bf',
+					pointBorderWidth: 1.5,
+					pointRadius: 5,
+				}
+			]
+
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						max: 2500,
+						min: 0,
+						stepSize: 500,
+					},
+				}],
+			},
+			responsive: true,
+			legend: {
+				display: false
+			}
+		}
+	});
+});
+
+// Monthly Line Chart
+monthly.click(function() {
+	// remove .active class from all buttons
+	for (i = 0; i < buttonArray.length; i++) {
+		buttonArray[i].removeClass("active");
+	}
+
+	// add .active class
+	monthly.addClass("active");
+
+	var trafficChart = new Chart(traffic, {
+		type: 'line',
+		data: {
+			labels: ["", "16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31", ""],
+			datasets: [{
+				data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000],
+				backgroundColor: [
+					'#e28626',
+				],
+				borderColor: [
+					'rgba(115, 119, 191,1)'
+				],
+				borderWidth: 1,
+				lineTension: 0,
+				pointBackgroundColor: 'white',
+				pointBorderColor: '#7377bf',
+				pointBorderWidth: 1.5,
+				pointRadius: 5,
+				}
+					]
+
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						max: 2500,
+						min: 0,
+						stepSize: 500,
+					},
+				}],
+			},
+			responsive: true,
+			legend: {
+				display: false
+			}
+		}
+	});
+
+}); //end monthly traffic line chart
+
+
+
+/* Daily Traffic Bar Chart */
 var dailyTraffic = document.getElementById("dailyTrafficChart");
 
 var dailyTrafficChart = new Chart(dailyTraffic, {
