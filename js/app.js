65c82baf8b9fc9f6e72c9b52111525f227b12b08
Nicoal Price <nicoalprice@gmail.com>
2016-11-10 13:10:05 -0500

    added autocomplete for member search

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ $("#close-button").click(function() {
 
 
 /* Use JS to allow you to submit the form and display a confirmation the message was sent. You won't actually submit the form, just simulate the action using JavaScript.*/
-function sendMessage() {
-	var sendButton = document.getElementById("send-button");
-//when user clicks send
-// pop-up message
-// text "Your message has been sent."
-}
+//function sendMessage() {
+//	var sendButton = document.getElementById("send-button");
+////when user clicks send
+//// pop-up message
+//// text "Your message has been sent."
+//}
 
 
 /* Use JS to display error messages if user isn’t selected or message field is empty.*/
@@ -33,3 +33,17 @@ function sendMessage() {
 //	//icon turns white
 //	//display green bar next to selected icon
 //}
+
+/* Auto-Complete Member Search */
+
+$('input[id="search"]').autoComplete({
+	minChars: 1,
+	source: function(term, suggest){
+		term = term.toLowerCase();
+		var choices = ['Bucky Bibbler', 'Abraham Lincoln', 'Sheri Berry', 'Joan Pennington', 'Hillary Clinton', 'Erik Estrada', 'Jacob Jacobson', 'Weird Al Yankovich', 'Mickey Mouse', 'John Boy', 'Babe Ruth', 'Dolly Parton', 'Scout Finch'];
+		var matches = [];
+		for (i=0; i<choices.length; i++)
+			if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+		suggest(matches);
+	}
+});
